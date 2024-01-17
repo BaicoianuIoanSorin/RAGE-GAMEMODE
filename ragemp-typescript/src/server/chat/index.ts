0c1ce7c996a33cb6c9fb8ce3e3b6b5a4998b3803
Baicoianu Ioan-Sorin <71268639+BaicoianuIoanSorin@users.noreply.github.com>
2024-01-17 16:33:49 +0100

    made range a constant

--- ragemp-typescript/src/server/chat/index.ts
+++ ragemp-typescript/src/server/chat/index.ts
@@ -2,6 +2,8 @@ import { ChatEvents } from '@shared/chat/events.constants';
 import { ChatMessage } from '@shared/chat/model';
 import * as rpc from 'rage-rpc';
 
+const range: number = 15;
+
 rpc.register(ChatEvents.SERVER_CHAT_MESSAGE, (chatMessageJSON) => {
     console.log(`${ChatEvents.SERVER_CHAT_MESSAGE} -> chatMessageJSON: ${chatMessageJSON}`);
 
@@ -12,7 +14,7 @@ rpc.register(ChatEvents.SERVER_CHAT_MESSAGE, (chatMessageJSON) => {
 
     console.log(`${ChatEvents.SERVER_CHAT_MESSAGE} -> player.name: ${player.name}`);
     
-    mp.players.forEachInRange(player.position, 15, (playerInRange) => {
+    mp.players.forEachInRange(player.position, range, (playerInRange) => {
         rpc.callClient(playerInRange, ChatEvents.CLIENT_RECEIVE_MESSAGE, chatMessageJSON);
     });
 });
